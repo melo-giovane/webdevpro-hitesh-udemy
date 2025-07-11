7c4d2f22f8561ab5426d2f6da3c8adf69310c206
Giovane <giovanec.melo@hotmail.com>
2025-07-11 15:51:45 -0300

    Adds refresh token endpoint for user authentication
    Implements a controller to handle access token renewal using refresh tokens,
    improving authentication flow and session management. Lays groundwork for secure logout and token rotation.

--- vidtube/src/controllers/user.controllers.js
+++ vidtube/src/controllers/user.controllers.js
@@ -6,6 +6,7 @@ import {
   deleteFromCloudinary,
 } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import jwt from "jsonwebtoken"
 
 const generateAccessAndRefreshToken = async (userId) => {
   try {
@@ -171,7 +172,63 @@ const loginUser = asyncHandler(async (req, res) => {
 
 });
 
+const logoutUser = asyncHandler( async (req, res) => {
+  await User.findByIdAndUpdate(
+    //TODO: need to come back here after middleware
+  )
+})
+
+const refreshAccessToken = asyncHandler( async (req, res) =>
+{
+  const incomingRefreshToken = req.cookies.refreshAccessToken || req.body.refreshToken
+
+  if (!incomingRefreshToken) {
+    throw new ApiError(401, "Refresh token is required")
+  }
+
+  try {
+    const decodedToken = jwt.verify(
+      incomingRefreshToken,
+      process.env.REFRESH_TOKEN_SECRET
+    )
+    const user = await User.findById(decodedToken?._id)
+
+
+    if (!user) {
+      throw new ApiError(401, "Invalid refresh token")
+    }
+
+    if (incomingRefreshToken !== user?.refreshToken) {
+      throw new ApiError(401, "Invalid refresh token")
+    }
+
+    const options = {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+    }
+
+    const {accessToken, refreshToken: newRefreshToken} = await generateAccessAndRefreshToken(user._id)
+
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", newRefreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        {accessToken,
+          refreshToken: newRefreshToken},
+          "Access token refreshed successfully"
+      ))
+
+  } catch (error) {
+    throw new ApiError(500, "Something went wrong while refreshing access token")
+  }
+
+})
+
 export {
   registerUser,
-  loginUser
+  loginUser,
+  refreshAccessToken
  };
