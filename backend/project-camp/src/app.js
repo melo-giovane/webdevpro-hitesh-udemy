31426624f78c3780ab788bebaafa1e6b1b206336
Giovane <giovanec.melo@hotmail.com>
2025-10-01 01:26:17 -0300

    api response and  api error classese added

--- backend/project-camp/src/app.js
+++ backend/project-camp/src/app.js
@@ -1,8 +1,21 @@
 import express from "express";
-
+import cors from "cors";
 const app = express();
 
+//basic configurations
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+
+//cors configs
+app.use(cors({
+  origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+  credentials: true,
+  methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Authorization", "Content-Type"]
+}));
+
 app.get("/", (req, res) => {
-  res.send("Welcome to basecampy")
-})
+  res.send("Welcome to basecampy");
+});
 export default app;
