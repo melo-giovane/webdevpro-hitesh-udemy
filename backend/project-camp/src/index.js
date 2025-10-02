dbab7eb135634ae1d208f8b2309cec476eb5db35
Giovane <giovanec.melo@hotmail.com>
2025-10-02 00:41:54 -0300

    refactor: move database connection logic into index.js

--- backend/project-camp/src/index.js
+++ backend/project-camp/src/index.js
@@ -1,11 +1,19 @@
 import dotenv from "dotenv";
-import app from "./app.js"
+import app from "./app.js";
+import connectDB from "./db/index.js";
 dotenv.config({
   path: "./.env",
 });
 
 const port = process.env.PORT || 4000;
 
-app.listen(port, () => {
-  console.log(`Example app listening on http://localhost:${port}`);
-});
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Example app listening on http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error", err);
+    process.exit;
+  });
