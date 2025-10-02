d877050302260cbeba30ded1555570bdc19cede9
Giovane <giovanec.melo@hotmail.com>
2025-10-02 16:06:56 -0300

    feat: implement async error handling middleware and update healthcheck controller

--- backend/project-camp/src/controllers/healthcheck.controllers.js
+++ backend/project-camp/src/controllers/healthcheck.controllers.js
@@ -1,11 +1,14 @@
 import { ApiResponse } from "../utils/api-response.js";
 
-const healthcheck = (req, res) => {
+/* const healthcheck = async (req, res, next) => {
   try {
+    const user = await get.UserFromDB()
     res
-      .status(200)
-      .json(new ApiResponse(200, { message: "Server is running" }));
-  } catch (error) {}
-};
+    .status(200)
+    .json(new ApiResponse(200, { message: "Server is running" }));
+  } catch (error) {
+    next(err)
+  }
+} */
 
-export { healthcheck };
+export {healthcheck}
