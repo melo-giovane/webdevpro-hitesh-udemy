e047dd82ef8202b722580f906a2683f0ce04ddb8
Giovane <giovanec.melo@hotmail.com>
2025-10-04 10:28:30 -0300

    feat: add jsonwebtoken for token generation and implement access/refresh token methods in user model

--- backend/project-camp/src/models/user.models.js
+++ backend/project-camp/src/models/user.models.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
@@ -61,15 +62,39 @@ const userSchema = new Schema(
   },
 );
 
-userSchema.pre("save", async function(next){
-  if(!this.isModified("password")) return next()
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
 
-  this.password = await bcrypt.hash(this.password, 10)
-  next()
-})
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
 
-userSchema.methods.isPasswordCorrect = async function (password){
- return await bcrypt.compare(password, this.password)
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function() {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {expiresIn: process.env.ACCESS_TOKEN_EXPIRY}
+  )
+}
+
+userSchema.geneateRefreshToken = function(){
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {expiresIn: process.env.REFRESH_TOKEN_EXPIRY}
+  )
 }
 
 export const User = mongoose.model("User", userSchema);
