42a9bdfa1e09840cc3ec5ddb41cd88b1f94cc24d
Giovane <giovanec.melo@hotmail.com>
2025-10-06 15:36:59 -0300

    Initialize Vite + React project with basic setup, including package.json, App component, main entry point, Vite configuration, and SVG assets.

---
+++ react-course/01basicvite/src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import MyApp from './App.jsx'
+
+
+createRoot(document.getElementById('root')).render(
+  MyApp()
+
+)
+
+
