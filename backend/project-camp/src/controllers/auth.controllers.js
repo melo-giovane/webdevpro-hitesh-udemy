b3b6bc3aef7d8c500e018606968cf5521bcb191f
Giovane <giovanec.melo@hotmail.com>
2025-10-09 16:40:49 -0300

    feat: adicionar função para gerar tokens de acesso e refresh

--- backend/project-camp/src/controllers/auth.controllers.js
+++ backend/project-camp/src/controllers/auth.controllers.js
@@ -3,6 +3,23 @@ import { ApiResponse } from "../utils/api-response.js";
 import { ApiError } from "../utils/api-error.js";
 import { asyncHandler } from "../utils/async-handler.js";
 
+const generateAccessAndRefreshTokens = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(
+      500,
+      "Something went wrong while generating access token",
+    );
+  }
+};
+
 const registerUser = asyncHandler(async (req, res) => {
   const { email, username, password, role } = req.body;
 
