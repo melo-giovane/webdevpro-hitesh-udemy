1b75f119ee2a3b0a901fc7fb2b5cb14ef42fecfe
Giovane <giovanec.melo@hotmail.com>
2025-10-09 00:26:19 -0300

    refactor: melhorar a formatação e a legibilidade do código na função registerUser

--- backend/project-camp/src/controllers/auth.controllers.js
+++ backend/project-camp/src/controllers/auth.controllers.js
@@ -3,25 +3,24 @@ import { ApiResponse } from "../utils/api-response.js";
 import { ApiError } from "../utils/api-error.js";
 import { asyncHandler } from "../utils/async-handler.js";
 
-
-
 const registerUser = asyncHandler(async (req, res) => {
- const {email, username, password, role} = req.body
+  const { email, username, password, role } = req.body;
 
- const existedUser = await User.findOne({
-  $or: [{username}, {email}]
- })
+  const existedUser = await User.findOne({
+    $or: [{ username }, { email }],
+  });
 
- if(existedUser) {
-  throw new ApiError(409, "User with email or username already exists", [])
- }
+  if (existedUser) {
+    throw new ApiError(409, "User with email or username already exists", []);
+  }
 
- const user = await User.create({
-  email,
-  password,
-  username,
-  isEmailVerified: false
- })
+  const user = await User.create({
+    email,
+    password,
+    username,
+    isEmailVerified: false,
+  });
 
-
-})
+  const { unHashedToken, hashedToken, tokenExpiry } =
+    user.generateTemporaryToken();
+});
