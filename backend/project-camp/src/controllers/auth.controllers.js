a7bca90e3401cd422042abac21f9be9b9677f4d5
Giovane <giovanec.melo@hotmail.com>
2025-10-09 00:10:17 -0300

    feat: implementar a função de registro de usuário com verificação de duplicidade

---
+++ backend/project-camp/src/controllers/auth.controllers.js
@@ -0,0 +1,18 @@
+import { User } from "../models/user.models.js";
+import { ApiResponse } from "../utils/api-response.js";
+import { ApiError } from "../utils/api-error.js";
+import { asyncHandler } from "../utils/async-handler.js";
+
+
+
+const registerUser = asyncHandler(async (req, res) => {
+ const {email, username, password, role} = req.body
+
+ const existedUser = await User.findOne({
+  $or: [{username}, {email}]
+ })
+
+ if(existedUser) {
+  throw new ApiError(409, "User with email or username already exists", [])
+ }
+})
