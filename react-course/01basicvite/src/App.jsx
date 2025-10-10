9f42007ecc57cd1b09e8626c9193c201ab8c2c53
Giovane <giovanec.melo@hotmail.com>
2025-10-10 02:14:00 -0300

    feat: atualizar estrutura do App e adicionar novos elementos de exemplo

--- react-course/01basicvite/src/App.jsx
+++ react-course/01basicvite/src/App.jsx
@@ -1,10 +1,11 @@
 import Youtube from "./Youtube.jsx";
 
+const username = 'giovane'
 function App() {
   return (
     <>
-      <h1>Vite react app</h1>
-      <h1>Vite react app</h1>
+      <h1>Vite react app {2 + 2}</h1>
+      <h1>Vite react app {username}</h1>
       <Youtube />
     </>
   );
--- react-course/01basicvite/src/main.jsx
+++ react-course/01basicvite/src/main.jsx
@@ -1,7 +1,38 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+const reactElement = {
+  type: "a",
+  props: {
+    href: "http://google.com",
+    target: "_blank",
+  },
+  children: "Click me to visit google",
+};
+
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom React App</h1>
+    </div>
+  );
+}
+const AnotherElement = (
+  <a href="http://google.com" target="_blank">
+    Visit Google
+  </a>
+);
+
+const AreactElement = React.createElement(
+  "a",
+  { href: "https://google.com", target: "_blank" },
+  "Click to visit google"
+);
+
+createRoot(document.getElementById("root")).render(
+
     <App />
-)
+
+);
