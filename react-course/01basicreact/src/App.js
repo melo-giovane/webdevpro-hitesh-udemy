6b6a7f07edf63f17b9adef58c805344c65a886e9
Giovane <giovanec.melo@hotmail.com>
2025-10-10 01:27:11 -0300

    feat: adicionar componente Youtube e atualizar estrutura do App

--- react-course/01basicreact/src/App.js
+++ react-course/01basicreact/src/App.js
@@ -1,6 +1,11 @@
+import Youtube from "./Youtube.jsx";
+
 function App() {
   return (
-    <h2>Classic react app</h2>
+    <>
+      <h2>Classic react app</h2>
+      <Youtube />
+    </>
   );
 }
 
