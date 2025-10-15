633f766a9c7d21c9441703b932fab73123fe46a4
Giovane <giovanec.melo@hotmail.com>
2025-10-14 23:50:14 -0300

    feat: implementar rota de logout e limpar cookies de autenticação

--- backend/project-camp/src/routes/auth.routes.js
+++ backend/project-camp/src/routes/auth.routes.js
@@ -1,15 +1,18 @@
 import { Router } from "express";
-import { login, registerUser } from "../controllers/auth.controllers.js";
+import { login, registerUser, logoutUser } from "../controllers/auth.controllers.js";
 import { validate } from "../middlewares/validator.middlewares.js";
 import {
     userRegisterValidator,
     userLoginValidator,
 } from "../validators/index.js";
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
 const router = Router();
 
 router.route("/register").post(userRegisterValidator(), validate, registerUser);
 router.route("/login").post(userLoginValidator(), validate, login);
-router.route("/logout").post(login);
+
+//secure routes
+router.route("/logout").post(verifyJWT, logoutUser);
 
 export default router;
