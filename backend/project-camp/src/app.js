a853cd01dc9cccd8b97ee3b0d21f1d1edd06d11d
Giovane <giovanec.melo@hotmail.com>
2025-10-14 22:26:07 -0300

    feat: adicionar middleware de verificação de JWT

--- backend/project-camp/src/app.js
+++ backend/project-camp/src/app.js
@@ -1,8 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-
-
+import { verifyJWT } from "./middlewares/auth.middlewares.js";
 
 const app = express();
 
@@ -10,8 +9,10 @@ const app = express();
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
-app.use(cookieParser())
+app.use(cookieParser());
 
+//middlewares
+app.use(verifyJWT);
 
 //cors configs
 app.use(
@@ -27,6 +28,7 @@ app.use(
 
 import healthcheckRouter from "./routes/healthcheck.routes.js";
 import authRouter from "./routes/auth.routes.js";
+import { verifyJWT } from "./middlewares/auth.middlewares.js";
 
 app.use("/api/v1/healthcheck", healthcheckRouter);
 app.use("/api/v1/auth", authRouter);
