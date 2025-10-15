95657f4a054b00824c1410215167883226c4a38d
Giovane <giovanec.melo@hotmail.com>
2025-10-14 23:23:53 -0300

    refactor: remover importação não utilizada do middleware de verificação de JWT

--- backend/project-camp/src/app.js
+++ backend/project-camp/src/app.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import { verifyJWT } from "./middlewares/auth.middlewares.js";
 
 const app = express();
 
